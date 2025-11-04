2ab063c8750f581f404e4c7612d92554d5fadd11
DeMarcus Cousins IV <asiynbusiness@gmail.com>
2025-11-04 15:05:12 +0100

    Styling och navbar komponent

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Image from "next/image";
+import logo from "../../images/student_event_logo_1.png";
+import styles from "./navbar.module.css";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Navbar() {
+  const pathName = usePathname();
+
+  const getLinkClass = (href: string) => {
+    const isActive =
+      href === "/" ? pathName === "/" : pathName?.startsWith(href);
+    return `${styles["nav-link"]} ${isActive ? styles.active : ""}`.trim();
+  };
+
+  return (
+    <nav className={styles.navbar}>
+      <Link href="/" className={styles.logo}>
+        <Image
+          src={logo}
+          alt="Student Event logo"
+          className={styles["nav-img"]}
+          aria-hidden={true}
+          priority
+        />
+      </Link>
+
+      <div className={styles["nav-buttons-container"]}>
+        <Link href="/" className={getLinkClass("/")}>
+          Hem
+        </Link>
+        <Link href="/calendar" className={getLinkClass("/calendar")}>
+          Kalender
+        </Link>
+        <Link href="/create-event" className={getLinkClass("/create-event")}>
+          Skapa Event
+        </Link>
+      </div>
+    </nav>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+
+      <Navbar />
+
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        <h1>Student Event</h1>
         <ol>
           <li>
             Get started by editing <code>src/app/page.tsx</code>.
