b08f6fa36143754ecc6ab2506afcb7ddf9275660
DeMarcus Cousins IV <asiynbusiness@gmail.com>
2025-11-05 10:56:09 +0100

    Färger

--- src/app/calendar/selectFilter.tsx
+++ src/app/calendar/selectFilter.tsx
@@ -1,4 +1,6 @@
 import styles from "./filter.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import the macro
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 
 type Option = {
   value?: string;
@@ -13,35 +15,45 @@ type SelectFilterProps = {
   options: Option[];
 };
 
-export default function SelectFilter({ label, id, options }: SelectFilterProps) {
+export default function SelectFilter({
+  label,
+  id,
+  options,
+}: SelectFilterProps) {
   return (
     <div className={styles["filter-group"]}>
       <label className={styles["filter-label"]} htmlFor={id}>
         {label}
       </label>
-      <select
-        className={styles["filter-dropdown"]}
-        id={id}
-        name={id}
-        multiple
-        size={1}
-      >
-        {options.map((opt) =>
-          opt.optgroup ? (
-            <optgroup key={opt.optgroup} label={opt.optgroup}>
-              {opt.items?.map((item) => (
-                <option key={item.value} value={item.value}>
-                  {item.label}
-                </option>
-              ))}
-            </optgroup>
-          ) : (
-            <option key={opt.value} value={opt.value}>
-              {opt.label}
-            </option>
-          )
-        )}
-      </select>
+      <div className={styles["select-wrapper"]}>
+        <select
+          className={styles["filter-dropdown"]}
+          id={id}
+          name={id}
+          multiple
+          size={1}
+        >
+          {options.map((opt) =>
+            opt.optgroup ? (
+              <optgroup key={opt.optgroup} label={opt.optgroup}>
+                {opt.items?.map((item) => (
+                  <option key={item.value} value={item.value}>
+                    {item.label}
+                  </option>
+                ))}
+              </optgroup>
+            ) : (
+              <option key={opt.value} value={opt.value}>
+                {opt.label}
+              </option>
+            )
+          )}
+        </select>
+        <FontAwesomeIcon
+          icon={faCaretDown}
+          className={styles["dropdown-icon"]}
+        />
+      </div>
     </div>
   );
 }
