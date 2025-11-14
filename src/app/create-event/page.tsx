4a9b465dc41d9bfb2dae5284853479cdb42fbac4
MimmAm1 <williamlundgrenj@gmail.com>
2025-11-14 13:17:39 +0100

    grid skapa event

--- src/app/create-event/page.tsx
+++ src/app/create-event/page.tsx
@@ -1,20 +1,34 @@
 import Navbar from "../components/navbar";
-import styles from "../page.module.css";
+import styles from "./loginPage.module.css";
 
 export const metadata = {
   title: "Skapa Event | StudentEvent",
 };
 
 export default function CreateEvent() {
-    return (
-        <>
-        <Navbar />
-        <div className={styles.page}>
-            <main>
-                <h1>Skapa Event</h1>
-                <p>Här kan du skapa ett nytt event.</p>
-            </main>
+  return (
+    <>
+      <Navbar />
+
+      <div className={styles.grid}>
+        <div className={styles.box}></div>
+        <div className={styles.box}></div>
+        <div className={styles.box}></div>
+
+        <div className={styles.box}></div>
+        <div className={styles.box}>
+            Content
+            din mammas håriga
+
+
         </div>
-        </>
-    );
+        <div className={styles.box}></div>
+
+        <div className={styles.box}></div>
+        <div className={styles.box}></div>
+        <div className={styles.box}></div>
+      </div>
+    </>
+  );
 }
+
