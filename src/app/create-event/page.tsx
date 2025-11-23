3c28b6e3cd38005bb3d58654f077204df80f8ae7
gurkan222 <esegersson@gmail.com>
2025-11-23 13:14:03 +0100

    Create event CSS
    fixat basic css till formulär

--- src/app/create-event/page.tsx
+++ src/app/create-event/page.tsx
@@ -9,26 +9,125 @@ export default function CreateEvent() {
   return (
     <>
       <Navbar />
-
-      <div className={styles.grid}>
-        <div className={styles.box}></div>
-        <div className={styles.box}></div>
-        <div className={styles.box}></div>
-
-        <div className={styles.box}></div>
-        <div className={styles.box}>
-            Content
-            din mammas håriga
-
-
+      <div className={styles.container}>
+        <div className={styles.grid}>
+          <div
+            className={styles.headFlex}
+            style={{ gridRow: "1", gridColumn: "1 / span 2" }}
+          >
+            <p className={styles.headText}>skapa ditt event:</p>
+          </div>
+          <div
+            className={styles.bodyFlex}
+            style={{ gridRow: "2", gridColumn: "1" }}
+          >
+            <p className={styles.bodyText}>namn på event:</p>
+          </div>
+          <div
+            className={styles.bodyFlex}
+            style={{ gridRow: "3", gridColumn: "1" }}
+          >
+            <p className={styles.bodyText}>datum:</p>
+          </div>
+          <div
+            className={styles.bodyFlex}
+            style={{ gridRow: "4", gridColumn: "1" }}
+          >
+            <p className={styles.bodyText}>tid:</p>
+          </div>
+          <div
+            className={styles.bodyFlex}
+            style={{ gridRow: "5", gridColumn: "1" }}
+          >
+            <p className={styles.bodyText}>plats:</p>
+          </div>
+          <div
+            className={styles.bodyFlex}
+            style={{ gridRow: "6", gridColumn: "1" }}
+          >
+            <p className={styles.bodyText}>värd:</p>
+          </div>
+          <div
+            className={styles.descFlex}
+            style={{ gridRow: "7", gridColumn: "1 /span 2" }}
+          >
+            <p className={styles.bodyText}>beskrivning:</p>
+          </div>{" "}
+          <div
+            className={styles.descFlex}
+            style={{ gridRow: "8", gridColumn: "1 /span 2" }}
+          >
+            <p className={styles.bodyText}>---input--</p>
+          </div>{" "}
+          <div
+            className={styles.bodyFlex}
+            style={{ gridRow: "9", gridColumn: "1" }}
+          >
+            <p className={styles.bodyText}>omslag/ affisch:</p>
+          </div>{" "}
+          <div
+            className={styles.bodyFlex}
+            style={{ gridRow: "10", gridColumn: "1" }}
+          >
+            <p className={styles.bodyText}>fakultet:</p>
+          </div>{" "}
+          <div
+            className={styles.bodyFlex}
+            style={{ gridRow: "11", gridColumn: "1" }}
+          >
+            <p className={styles.bodyText}>sektion:</p>
+          </div>
+          {/* ------------------RIGHT SIDE----------------- */}
+          <div
+            className={styles.bodyFlexRHS}
+            style={{ gridRow: "2", gridColumn: "2" }}
+          >
+            <p className={styles.bodyText}>---input---</p>
+          </div>
+          <div
+            className={styles.bodyFlexRHS}
+            style={{ gridRow: "3", gridColumn: "2" }}
+          >
+            <p className={styles.bodyText}>---input---</p>
+          </div>
+          <div
+            className={styles.bodyFlexRHS}
+            style={{ gridRow: "4", gridColumn: "2" }}
+          >
+            <p className={styles.bodyText}>---input---</p>
+          </div>
+          <div
+            className={styles.bodyFlexRHS}
+            style={{ gridRow: "5", gridColumn: "2" }}
+          >
+            <p className={styles.bodyText}>---input---</p>
+          </div>
+          <div
+            className={styles.bodyFlexRHS}
+            style={{ gridRow: "6", gridColumn: "2" }}
+          >
+            <p className={styles.bodyText}>---input---</p>
+          </div>
+          <div
+            className={styles.bodyFlexRHS}
+            style={{ gridRow: "9", gridColumn: "2" }}
+          >
+            <p className={styles.bodyText}>---input---</p>
+          </div>{" "}
+          <div
+            className={styles.bodyFlexRHS}
+            style={{ gridRow: "10", gridColumn: "2" }}
+          >
+            <p className={styles.bodyText}>---input---</p>
+          </div>{" "}
+          <div
+            className={styles.bodyFlexRHS}
+            style={{ gridRow: "11", gridColumn: "2" }}
+          >
+            <p className={styles.bodyText}>---input---</p>
+          </div>
         </div>
-        <div className={styles.box}></div>
-
-        <div className={styles.box}></div>
-        <div className={styles.box}></div>
-        <div className={styles.box}></div>
       </div>
     </>
   );
 }
-
