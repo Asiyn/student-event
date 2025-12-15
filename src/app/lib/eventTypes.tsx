83b571ce23279eb35c0a5e693feba5728c4f993e
MimmAm1 <williamlundgrenj@gmail.com>
2025-12-15 17:01:41 +0100

    Update eventTypes.tsx

--- src/app/lib/eventTypes.tsx
+++ src/app/lib/eventTypes.tsx
@@ -13,3 +13,32 @@ export type EventFormData = {
   imageData?: string | null;
   color?: string | null;
 };
+
+export const DEFAULT_EVENTS: EventFormData[] = [
+  {
+    event: "VinterKravallen",
+    arrangor: "Fest-n",
+    date: "2025-08-25",
+    place: "Kårhuset Kollektivet",
+    startTime: "18:00",
+    endTime: "23:00",
+    fakultet: "LinTek",
+    beskrivning: "VinterKravall hela veckan!",
+    organizerURL: "https://lintek.liu.se",
+    imageData: "../images/02_VK.PNG",
+    color: "#FF9800",
+  },
+  {
+    event: "FullMoon",
+    arrangor: "SSKADAT",
+    date: "2025-09-12",
+    place: "Kårhuset Örat",
+    startTime: "19:00",
+    endTime: "23:30",
+    fakultet: "STUFF",
+    beskrivning: "Temasittning med tacos, underhållning och efterfest.",
+    organizerURL: "https://stuff.liu.se",
+    imageData: "../images/fullMoonSSkadat.jpg",
+    color: "#4CAF50",
+  },
+];
