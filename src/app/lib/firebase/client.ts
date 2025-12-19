7b979297f391ea4101719956a8b7c8e8fc979dd1
DeMarcus Cousins IV <asiynbusiness@gmail.com>
2025-12-19 20:48:08 +0100

    några firebase saker

--- src/app/lib/firebase/client.ts
+++ src/app/lib/firebase/client.ts
@@ -1,20 +1,26 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
 
+// Import the functions you need from the SDKs you need
+//import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDRQxQMbGWoS8bB--ru183-CkwtLaH6zTk",
-
-  authDomain: "lithe-event.firebaseapp.com",
-
-  projectId: "lithe-event",
-
-  storageBucket: "lithe-event.firebasestorage.app",
-
-  messagingSenderId: "474221934035",
-
-  appId: "1:474221934035:web:0a667a5ba3a81e483c6b26",
-
-  measurementId: "G-LDD4MJ4ZJW",
+  apiKey: "AIzaSyBEkXQdbvPoMy5b6ozQ1wFBbJXGoZYNaY0",
+  authDomain: "student-event-liu.firebaseapp.com",
+  projectId: "student-event-liu",
+  storageBucket: "student-event-liu.firebasestorage.app",
+  messagingSenderId: "591190866940",
+  appId: "1:591190866940:web:8badbf71eb11021bd09079",
+  measurementId: "G-63THMWF6XC"
 };
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// console.log(analytics);
 
 export const firebaseApp = getApps().length
   ? getApp()
